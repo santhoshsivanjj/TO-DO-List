aab8436d18b148e591847f26bc7e8de7c920be55
santhoshsivanjj <santhosh.sivan@jobinandjismi.com>
2024-01-04 13:36:44 +0530

    Changes in List

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -3,9 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const todoSlice = createSlice({
   name: "todos",
   initialState: [
-    { id: 1, title: "todo 1", completed: false },
-    { id: 2, title: "todo 2", completed: false },
-    { id: 3, title: "todo 3", completed: true },
   ],
   reducers: {
     addTodo: (state, action) => {
